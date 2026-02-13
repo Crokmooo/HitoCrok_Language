542a38362cfa2a5f159bef9e6db9511372f8d790
Jérémy Chény <jeremcheny@gmail.com>
2026-02-13 19:20:15 +0100

    IF and FOR statements added

--- TP1/calcBase.py
+++ TP1/calcBase.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 19 08:59:32 2024
 
-@author: FB
-"""
 import uuid
 import graphviz as gv
+import ply.lex as lex
+import ply.yacc as yacc
 
 reserved = {
     'print': 'PRINT',
@@ -15,7 +13,14 @@ reserved = {
     'while' : "WHILE",
 }
 
-# tokens = ( 'INF', 'EQUALEQUAL', 'EQUAL', 'NAME', 'NUMBER','MINUS', 'PLUS','TIMES','DIVIDE', 'LPAREN','RPAREN', 'OR', 'AND', 'SEMI' )
+precedence = (
+    ('left', 'OR'),
+    ('left', 'AND'),
+    ('nonassoc', 'INF', 'EQUALEQUAL'),
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'TIMES', 'DIVIDE')
+)
+
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
@@ -26,77 +31,60 @@ t_OR = r'\|'
 t_AND = r'\&'
 t_SEMI = r';'
 t_INF = r'<'
+t_SUP = r'>'
 t_EQUALEQUAL = r'=='
 t_EQUAL = r'='
 t_LBRACKET = r'\{'
 t_RBRACKET = r'\}'
-# t_NAME = r'[a-zA-Z_][a-zA-Z_0-9]*'
-tokens = ['INF', 'EQUALEQUAL', 'EQUAL', 'NAME', 'NUMBER', 'MINUS', 'PLUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN', 'OR',
+t_ignore = " \t"
+tokens = ['INF', 'EQUALEQUAL', 'EQUAL', 'NAME', 'NUMBER', 'MINUS', 'PLUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN', 'OR', 'SUP',
           'AND', 'SEMI', 'LBRACKET', 'RBRACKET'] + list(reserved.values())
-
 
 def t_NAME(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value, 'NAME')  # Check for reserved words
+    t.type = reserved.get(t.value, 'NAME')
     return t
-
 
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-
-t_ignore = " \t"
-
-
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
-
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-
-precedence = (
-    ('left', 'OR'),
-    ('left', 'AND'),
-    ('nonassoc', 'INF', 'EQUALEQUAL'),
-    ('left', 'PLUS', 'MINUS'),
-    ('left', 'TIMES', 'DIVIDE')
-)
-
 names = {}
-import ply.lex as lex
-
 lex.lex()
-
 
 def evalInst(p):
     if p == 'empty': return
     assert type(p) is tuple
-    if p[0] == 'if':
-        if evalExpr(p[1]): evalInst(p[2])
-        elif len(p) == 4 and p[3][0] == 'else':
-            evalInst(p[3][1])
-    if p[0] == "assign":
-        names[p[1]] = p[2]
-    if p[0] == 'bloc':
-        evalInst(p[1])
-        evalInst(p[2])
-    if p[0] == 'print':
-        print('CALC> ', evalExpr(p[1]))
-
+    match p[0]:
+        case 'for':
+            evalInst(p[1])
+            while evalExpr(p[2]) :
+                evalInst(p[4])
+                evalInst(p[3])
+        case 'if':
+            if evalExpr(p[1]): evalInst(p[2])
+            elif len(p) == 4 and p[3][0] == 'else':
+                evalInst(p[3][1])
+        case "assign":
+            names[p[1]] = evalExpr(p[2])
+        case 'bloc':
+            evalInst(p[1])
+            evalInst(p[2])
+        case 'print':
+            print('CALC> ', evalExpr(p[1]))
 
 def evalExpr(p):
     if type(p) is int : return p
-
-    if p[1] in names.keys():
-        p[1] = names[evalExpr(p[1])]
-    if p[2] in names.keys():
-        p[2] = names[evalExpr(p[2])]
+    if type(p) is str : return names[p]
 
     match p[0]:
         case '*':
@@ -112,7 +100,7 @@ def evalExpr(p):
         case '<' :
             return evalExpr(p[1]) < evalExpr(p[2])
         case '>' :
-            return evalExpr(p[1]) < evalExpr(p[2])
+            return evalExpr(p[1]) > evalExpr(p[2])
         case '&&' :
             return evalExpr(p[1]) and evalExpr(p[2])
         case '||' :
@@ -123,8 +111,7 @@ def printTreeGraph(t):
     graph = gv.Digraph(format='pdf')
     graph.attr('node', shape='circle')
     addNode(graph, t)
-    #graph.render(filename='img/graph') #Pour Sauvegarder
-    graph.view() #Pour afficher
+    graph.view()
 
 def addNode(graph, t):
     myId = uuid.uuid4()
@@ -141,114 +128,91 @@ def addNode(graph, t):
     return myId
 
 def p_start(p):
-    'start : bloc'
+    """start : bloc"""
     print(p[1])
     printTreeGraph(p[1])
     evalInst(p[1])
-    '''
-    def p_expression_binop_plus(p): 
-    'expression : expression PLUS expression' 
-    #p[0] = p[1] + p[3] 
-    p[0] = ('PLUS', p[1] , p[3]) 
-    '''
-
 
 def p_bloc(p):
-    '''bloc : bloc statement SEMI
-    | statement SEMI'''
-
+    """bloc : bloc statement SEMI
+            | statement SEMI"""
     if len(p) == 4:
         p[0] = ('bloc', p[1], p[2])
     else:
         p[0] = ('bloc', 'empty', p[1])
 
-
 def p_statement_assign(p):
-    'statement : NAME EQUAL expression'
-    #names[p[1]] = p[3]
+    """statement : NAME EQUAL expression"""
     p[0] = ("assign", p[1], p[3])
 
-
 def p_statement_print(p):
-    'statement : PRINT LPAREN expression RPAREN'
-    # print(p[3])
+    """statement : PRINT LPAREN expression RPAREN"""
     p[0] = ('print', p[3])
 
-
 def p_expression_binop_inf(p):
-    'expression : expression INF expression'
+    """expression : expression INF expression"""
     p[0] = ('<', p[1], p[3])
 
+def p_expression_binop_sup(p):
+    """expression : expression SUP expression"""
+    p[0] = ('>', p[1], p[3])
 
 def p_expression_binop_equal(p):
-    'expression : expression EQUALEQUAL expression'
+    """expression : expression EQUALEQUAL expression"""
     p[0] = ('==', p[1], p[3])
 
-
 def p_expression_binop_and(p):
-    'expression : expression AND expression'
+    """expression : expression AND expression"""
     p[0] = ('&&', p[1], p[3])
 
-
 def p_expression_binop_or(p):
-    'expression : expression OR expression'
+    """expression : expression OR expression"""
     p[0] = ('||', p[1], p[3])
 
-
 def p_expression_binop_plus(p):
-    'expression : expression PLUS expression'
-    # p[0] = p[1] + p[3]
+    """expression : expression PLUS expression"""
     p[0] = ('+', p[1], p[3])
 
-
 def p_expression_binop_times(p):
-    'expression : expression TIMES expression'
-    # p[0] = p[1] * p[3]
+    """expression : expression TIMES expression"""
     p[0] = (p[2], p[1], p[3])
 
-
 def p_expression_binop_divide_and_minus(p):
-    '''expression : expression MINUS expression
-     | expression DIVIDE expression'''
+    """expression : expression MINUS expression
+            | expression DIVIDE expression"""
     if p[2] == '-':
-        p[0] = p[1] - p[3]
+        p[0] = ('-', p[1], p[3])
     else:
-        p[0] = p[1] / p[3]
-
+        p[0] = ('/', p[1], p[3])
 
 def p_expression_group(p):
-    'expression : LPAREN expression RPAREN'
+    """expression : LPAREN expression RPAREN"""
     p[0] = p[2]
 
-
 def p_expression_number(p):
-    'expression : NUMBER'
+    """expression : NUMBER"""
     p[0] = p[1]
 
-
 def p_expression_name(p):
-    'expression : NAME'
-    # p[0] = names[p[1]]
+    """expression : NAME"""
     p[0] = p[1]
 
 def p_statement_else(p):
-    'statement : IF LPAREN expression RPAREN LBRACKET bloc RBRACKET ELSE LBRACKET bloc RBRACKET'
+    """statement : IF LPAREN expression RPAREN LBRACKET bloc RBRACKET ELSE LBRACKET bloc RBRACKET"""
     p[0] = ('if', p[3], p[6], ('else', p[10]))
 
 def p_statement_if(p):
-    '''statement : IF LPAREN expression RPAREN LBRACKET bloc RBRACKET'''
+    """statement : IF LPAREN expression RPAREN LBRACKET bloc RBRACKET"""
     p[0] = ('if', p[3], p[6])
 
+def p_statement_for(p):
+    """statement : FOR LPAREN statement SEMI expression SEMI statement RPAREN LBRACKET bloc RBRACKET"""
+    p[0] = ('for', p[3], p[5], p[7], p[10])
 
 def p_error(p): print("Syntax error in input!")
 
-
-import ply.yacc as yacc
-
 yacc.yacc()
-s = ('x=2;if (x==2) {   '
-     'if(1+1==2){'
-     'print(12);'
-     '};} else {print(3+10);};print(1+2);')
+s = ('for (x=5;x>1;x=x-1) { print(x); };'
+     'for (x=1;x<100;x=x+2) { print(x); };print(5/2);')
 
 yacc.parse(s)
