6d3f0aabb81c54cc3b37edb5ae433b4618ef6dcc
Jérémy Chény <jeremcheny@gmail.com>
2026-02-18 21:11:15 +0100

    Read input program from file instead of hardcoded string

--- projet/interpreter.py
+++ projet/interpreter.py
@@ -1,12 +1,8 @@
 from projet.parser import parser
 from projet.lexer import lexer
 
-s = '''
-a = 0;
-do {
-    print(a);
-    a = a + 1;
-} while (a <= 10);
-'''
+f = open("terminal.txt")
+s = f.read()
+f.close()
 
 parser.parse(s)
